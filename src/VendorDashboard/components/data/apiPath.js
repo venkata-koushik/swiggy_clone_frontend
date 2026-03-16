9028fe4d09122be150076d49508f252492804a1f
venkata-koushik <koushik.chinu.2007@gmail.com>
2026-03-16 18:44:40 +0530

    changed the api

--- src/VendorDashboard/components/data/apiPath.js
+++
@@ -1,3 +0,0 @@
-export const API_URL="http://localhost:4000"
-
-//export const API_URL="https://swizzy.onrender.com"
--- src/VendorDashboard/helpers/ApiPath.js
+++
@@ -1,4 +0,0 @@
-// const API_PATH="http://localhost:4000/"
-
-
- export const API_PATH="https://swizzy.onrender.com"
